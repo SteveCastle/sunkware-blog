7b8ef2a21868caa0517425ff680e0433e1d93704
Steve Castle <steve@tigerhaunt.com>
2018-12-01 15:34:38 -0800

    fix logo width

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import React from 'react';
+import { Link } from 'gatsby';
+import github from '../img/github-icon.svg';
+import logo from '../img/logo.svg';
 
 const Navbar = () => (
   <nav className="navbar is-transparent">
     <div className="container">
       <div className="navbar-brand">
         <Link to="/" className="navbar-item" title="Logo">
-          <img src={logo} alt="Sunkware" style={{ width: '88px' }} />
+          <img src={logo} alt="Sunkware" style={{ width: '30px' }} />
         </Link>
       </div>
       <div className="navbar-start">
@@ -33,6 +33,6 @@ const Navbar = () => (
       </div>
     </div>
   </nav>
-)
+);
 
-export default Navbar
+export default Navbar;
