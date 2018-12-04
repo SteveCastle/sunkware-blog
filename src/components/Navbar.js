465a80570975638f094f85db43d0b192d94a30f5
Steve Castle <steve@tigerhaunt.com>
2018-12-04 09:09:31 -0800

    update blog posts

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,12 +12,6 @@ const Navbar = () => (
         </Link>
       </div>
       <div className="navbar-start">
-        <Link className="navbar-item" to="/about">
-          About
-        </Link>
-        <Link className="navbar-item" to="/products">
-          Projects
-        </Link>
       </div>
       <div className="navbar-end">
         <a
